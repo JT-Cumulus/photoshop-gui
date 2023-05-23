cabbc152d975c855f4bd1412a1f679ade89073c3
jt-io <jiatong1998@yahoo.com>
2023-05-23 11:17:55 +0200

    first commit

---
+++ src/main/java/com/photoshop/photoshopgui/PhotoshopGuiApplication.java
@@ -0,0 +1,13 @@
+package com.photoshop.photoshopgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoshopGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoshopGuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photoshop/photoshopgui/PhotoshopGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoshop.photoshopgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoshopGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
